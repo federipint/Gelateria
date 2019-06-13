7ff564fa524bde6db4bc68e3d7c359586d75a21c
user <user@192.168.20.135>
2019-06-13 17:49:18 +0200

    implementazione gusto e gustoFactory

--- src/com/corso/gelateria/model/Gusto.java
+++ src/com/corso/gelateria/model/Gusto.java
@@ -1,5 +1,49 @@
 package com.corso.gelateria.model;
 
+import java.util.ArrayList;
+
 public class Gusto {
+	
+	private String nome;
+	private double prezzo;
+	private ArrayList<String> ingredienti;
+	private double quantità;
+	
+			
+	public Gusto(String nome, double prezzo, ArrayList<String> ingredienti, double quantità) {
+		this.nome = nome;
+		this.prezzo = prezzo;
+		this.ingredienti = ingredienti;
+		this.quantità = quantità;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	public ArrayList<String> getIngredienti() {
+		return ingredienti;
+	}
+	public void setIngredienti(ArrayList<String> ingredienti) {
+		this.ingredienti = ingredienti;
+	}
+	public double getQuantità() {
+		return quantità;
+	}
+	public void setQuantità(double quantità) {
+		this.quantità = quantità;
+	}
+	
+	
+	
+
+	
 
 }
--- src/com/corso/gelateria/model/GustoFactory.java
+++ src/com/corso/gelateria/model/GustoFactory.java
@@ -1,5 +1,37 @@
 package com.corso.gelateria.model;
 
+import java.util.ArrayList;
+
+
+
+
 public class GustoFactory {
 
+	private static GustoFactory singleton;
+	private ArrayList<Gusto> listaGusti;
+	
+	
+	
+	private GustoFactory() {
+		listaGusti= new ArrayList<Gusto>();
+		
+	}
+	public static GustoFactory getInstance() {
+		if(singleton == null) {
+			singleton = new GustoFactory();
+		}
+		
+		return singleton;
+	}
+	
+		
+	public void addGusto(String nome, double prezzo, String ingredienti, double quantità) { 
+		
+		Gusto gusto = new Gusto(nome, prezzo, ingredienti, quantità);
+		listaGusti.add(gusto);
+	}
+
+	
+	
+	
 }
