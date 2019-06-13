0b0cf4cfaea13a584b5293d8242a560e5e9032cc
Federica <Federica@192.168.20.144>
2019-06-13 17:48:05 +0200

    implementazione Cono e ConoFactory

--- src/com/corso/gelateria/model/Cono.java
+++ src/com/corso/gelateria/model/Cono.java
@@ -1,5 +1,26 @@
 package com.corso.gelateria.model;
 
-public class Cono {
+import java.util.List;
 
+public class Cono {
+	private double prezzo;
+	private List<String> gusti;
+	public Cono(double prezzo, List<String> gusti) {
+		this.prezzo=prezzo;
+		this.gusti=gusti;
+	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	public List<String> getGusti() {
+		return gusti;
+	}
+	public void setGusti(List<String> gusti) {
+		this.gusti = gusti;
+	}
+
+	
 }
--- src/com/corso/gelateria/model/ConoFactory.java
+++ src/com/corso/gelateria/model/ConoFactory.java
@@ -1,5 +1,31 @@
 package com.corso.gelateria.model;
 
-public class ConoFactory {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ConoFactory {
+		
+		private static ConoFactory singleton;
+		private List<Cono> listaConi;
+		
+		
+		
+		private ConoFactory() {
+			listaConi = new ArrayList<Cono>();
+			
+		}
+		
+		public static ConoFactory getInstance() {
+			if(singleton == null) {
+				singleton = new ConoFactory();
+			}
+			
+			return singleton;
+		}
+		
+		public void addCono(double prezzo, List<String> gusti) {
+			Cono newCono= new Cono(prezzo, gusti);
+			listaConi.add(newCono);
+		}
 }
+
